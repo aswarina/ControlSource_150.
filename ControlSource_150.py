af7522d090db31450093a2e84315a791bd6940da
aswarina <lyxsyyawa@gmail.com>
2026-09-21 11:32:49 +0700

    jawaban nomor 4

--- ControlSource_150.py
+++ ControlSource_150.py
@@ -35,3 +35,11 @@ while a <= n:
     print(a, end=" ")
     a, b = b, a + b
 print()
+
+##4
+n = int(input("Masukkan nilai n untuk angka ganjil: "))
+
+print("Angka ganjil:")
+for i in range(1, n + 1, 2):
+    print(i, end=" ")
+print()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,13 +1,6 @@
-##2
-num1 = int(input("Masukkan angka pertama: "))
-num2 = int(input("Masukkan angka kedua: "))
-num3 = int(input("Masukkan angka ketiga: "))
+n = int(input("Masukkan nilai n untuk angka ganjil: "))
 
-#  percabangan untuk menentukan angka terbesar
-if num1 >= num2 and num1 >= num3:
-    terbesar = num1
-elif num2 >= num1 and num2 >= num3:
-    terbesar = num2
-else:
-    terbesar = num3
-print(f"Angka terbesar di antara {num1}, {num2}, dan {num3} adalah: {terbesar}")
+print("Angka ganjil:")
+for i in range(1, n + 1, 2):
+    print(i, end=" ")
+print()
